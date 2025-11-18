5ef99df1aaf94b6c6e6ba2fab863ac47907ca7e4
Prashant Rajan <82665966+prashantnrajan143@users.noreply.github.com>
2025-11-18 23:13:32 +0530

    Implement function to find minimum number
    Added a function to find the minimum number from a list of inputs.

--- practice.py
+++ practice.py
@@ -1,1 +1,11 @@
-print(10 // 9)
+####### Example2: Finding the minmum numbers #####
+def funf(*numbers):
+    if len(numbers) == 0:
+        return None
+    min_num = numbers[0]
+    for num in numbers:
+        if num < min_num:
+            min_num = num
+        return min_num
+print(funf(87, 0, -12, 90))
+#output: 87 #now
